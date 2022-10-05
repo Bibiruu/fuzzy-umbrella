04f31cf1257a2b57d63e07d385935ca524c5921f
Maria <so.mariagloria@gmail.com>
2022-10-05 12:59:04 +0300

    Practice project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Front } from 'Front'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Front />
     </div>
   )
 }
---
+++ src/components/TaskForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const TaskForm = ({ newTodo, onNewTodoChange, onFormSubmit }) => {
+  return (
+    <form onSubmit={onFormSubmit}>
+      <h2>Welsome todo app! Type new task below</h2>
+      <textarea value={newTodo} onChange={onNewTodoChange} />
+      <button type="submit">Submit Form!</button>
+    </form>
+  )
+}
